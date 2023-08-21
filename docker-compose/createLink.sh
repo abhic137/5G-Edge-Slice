083c4177b3cca83af3ece5eccf8fd3d6ee809a11
V Abhishek Raj <46273637+abhic137@users.noreply.github.com>
2023-08-21 17:12:29 +0530

    Update createLink.sh

--- docker-compose/createLink.sh
+++ docker-compose/createLink.sh
@@ -2,28 +2,26 @@
 
 C1_NAME=$1
 C2_NAME=$2
-LINK_COUNTER=0
 
 createNS(){
-	C_ID=$1
-	pid=$(docker inspect -f '{{.State.Pid}}' ${C_ID})
-	CNAME=$(basename $(docker inspect --format='{{.Name}}' ${C_ID}))
-	mkdir -p /var/run/netns/
-	ln -sfT /proc/$pid/ns/net /var/run/netns/${CNAME}
+    C_ID=$1
+    pid=$(docker inspect -f '{{.State.Pid}}' ${C_ID})
+    CNAME=$(basename $(docker inspect --format='{{.Name}}' ${C_ID}))
+    mkdir -p /var/run/netns/
+    ln -sfT /proc/$pid/ns/net /var/run/netns/${CNAME}
 }
 
 addLink(){
-	C1=$1
-	C2=$2
-	LINK_ID="dcp${LINK_COUNTER}"
-	((LINK_COUNTER++))
-	ip link add veth1_l type veth peer veth1_r
-	ip link set veth1_l netns ${C1}
-	ip link set veth1_r netns ${C2}
-	ip netns exec ${C1} ip l set veth1_l name ${LINK_ID}
-	ip netns exec ${C2} ip l set veth1_r name ${LINK_ID}
-	ip netns exec ${C1} ip l set ${LINK_ID} up
-	ip netns exec ${C2} ip l set ${LINK_ID} up
+    C1=$1
+    C2=$2
+    LINK_ID=$(uuidgen | cut -d '-' -f1)  # Generate a UUID and use the first part as the interface name
+    ip link add veth1_l type veth peer veth1_r
+    ip link set veth1_l netns ${C1}
+    ip link set veth1_r netns ${C2}
+    ip netns exec ${C1} ip l set veth1_l name ${LINK_ID}
+    ip netns exec ${C2} ip l set veth1_r name ${LINK_ID}
+    ip netns exec ${C1} ip l set ${LINK_ID} up
+    ip netns exec ${C2} ip l set ${LINK_ID} up
 }
 
 C1_ID=$(docker inspect --format="{{.Id}}" ${C1_NAME})
